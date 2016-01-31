6d1963cef38698a98a4b6fb277e73f3b5a3c6c4c
Zeriuno <d.pitrolo@gmx.com>
2016-01-31 17:36:53 +0100

    Presque rien
    C'est déjà -presque- beaucoup

---
+++ hotel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+  int choix = 0 ;
+  printf("Bienvenue dans le programme de gestion des réservations.\n") ;
+  while(choix != 9)
+  {
+    
+  }
+  printf("Au revoir.");
+}
