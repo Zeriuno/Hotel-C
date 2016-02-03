3d9e99d7e8ac44ebd87206a0bc7bdebaed967017
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-02-03 15:27:23 +0100

    Saisie des services

--- hotel.c
+++ hotel.c
@@ -11,30 +11,20 @@
 */
 
 #include <stdio.h>
-
+#include <string.h>
 /*Constantes*/
 
 #define MAX_CAT_SERV 20 /*Constante qui limite le catalogue des services*/
-
+#define MAX_NOM_SERVICE 21
 
 /*Déclarations préliminaires*/
 
-void catalogue_services_menu(); /*Ajout de la procédure pour afficher le menu qui montre les choix sur le catalogue de services*/
-void mauvais_choix(int par_choisi);
+void catalogue_services_menu()                   ; /*Ajout de la procédure pour afficher le menu qui montre les choix sur le catalogue de services*/
+void saisie_services()                           ;
+void enreg_catalogue_services()                  ;
 
-/*Variables locales*/
-struct service{            /*Service additionnel facturé par l'hôtel, référence du catalogue*/
-  float prix_service ;
-  char  nom_service  ;
-};
-struct service catalogue_services[MAX_CAT_SERV];  /*Catalogue listant les services*/
-struct frais{
-  int datefrais        ; /*autrement on en fera une string de 9, 'aaaammjj' (8) + '\0'*/
-  float montantfrais   ;
-  int codefrais[200] ;
-};
-
-
+void mauvais_choix(int par_choisi)               ;
+/*Variables globales*/
 
 struct reservation{
   /*code de réservation, à déterminer comment il est construit*/
@@ -45,6 +35,21 @@ struct reservation{
   char telclient[13]  /*+33653332003 qui peut être affiché +33 6 53 33 20 03. Vérifier de quelle taille doit être le numéro: 12?*/
 };
 
+
+  /*ici on parle du catalogue_services*/
+
+int sauv_catalogue_services = 0; /*Pour contrôler s'il faut sauvegarder le catalogue_services. Si c'est 0, le catalogue n'est pas à sauvegarder, si c'est 1 il faut le sauvegarder.*/
+int nb_services = 0 ;
+struct service{            /*Service additionnel facturé par l'hôtel, référence du catalogue*/
+  float prix_service ;
+  char  nom_service[]  ;
+};
+struct service catalogue_services[MAX_CAT_SERV];  /*Catalogue listant les services*/
+struct frais{
+  int datefrais        ; /*autrement on en fera une string de 9, 'aaaammjj' (8) + '\0'*/
+  float montantfrais   ;
+  int codefrais[200] ;
+};
 
 main()
 {
@@ -105,6 +110,7 @@ void catalogue_services_menu(){
     printf("1 - Lire le catalogue des services\n")         ;
     printf("2 - Modifier le catalogue des services\n")     ;
     printf("3 - Ajouter un service\n")                     ;
+    printf("4 - Supprimer un service\n")                   ;
     printf("9 - Quitter et revenir au menu principal\n\n") ;
 
     printf("Choisissez une action : ")                     ;
@@ -112,9 +118,19 @@ void catalogue_services_menu(){
     switch(choix_cat)
     {
       case 1:
-        /*f1 = fopen("catalogue-services.txt", "r") ; À modulariser*/
+        /*f1 = fopen("catalogue-services.txt", "r") ; À modulariser
+        fclose(f1);*/
         break;
       case 2:
+       /*f1 = fopen("catalogue-services.txt", "w") ; À modulariser
+        fclose(f1);*/
+        break;
+      case 3:
+       saisie_services();
+        break;
+      case 4:
+      /*f1 = fopen("catalogue-services.txt", "w") ; À modulariser
+        fclose(f1);*/
         break;
       case 9:
         break;
@@ -127,4 +143,32 @@ void catalogue_services_menu(){
 
 void mauvais_choix(int par_choix){
   printf("Vous avez sélectionné %d : ce choix n'est pas disponible. Veuillez ressaisir.\n", par_choix);
+}
+
+void saisie_services(){
+  int i = nb_services ;
+
+  strcpy(service.nom_service, "début")          ;
+  while(strcmp(service.nom_service,"fin") != 0)
+  {
+    printf("Saisir le nom du service ('fin' pour terminer) : ") ;
+    scanf("%s", service.nom_service)                            ;
+    if(strcmp(service.nom_service,"fin") = 0)
+    {
+      printf("Fin de la saisie. Retour au menu précédent.\n") ;
+    }
+    else
+    {
+      printf("Saisir le prix : ")       ;
+      scanf("%f", service.prix_service) ;
+      catalogue_services[i++] = service ;
+    }
+  }
+  if(i > 0)
+  {
+    nb_services = i                                ;
+    printf("%d services présents.\n", nb_services) ;
+    sauv_catalogue_services = 1                    ;
+    enreg_catalogue_services()                     ;
+  }
 }
