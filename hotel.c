b555daeb3143d91c8176561c44024b1f24bd216a
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-02-04 14:40:31 +0100

    Suivi des versions

--- hotel.c
+++ hotel.c
@@ -10,6 +10,7 @@
 * v 0.0.2 - 2016-02-03 Saisie et enregistrement du catalogue des services
 * v 0.1.0 - 2016-02-03 Sous-menu pour les services complet
 * v 0.1.1 - 2016-02-04 On s'attaque aux chambres
+* v 0.1.1 - 2016-02-04 Ebauches pour la réservation
 */
 
 #include <stdio.h>
