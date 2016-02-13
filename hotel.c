a8ed5186e0bd7414d416d80a9c0f80a6cccd9ac0
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-02-13 16:01:52 +0100

    rech_periode appellé - pas codé

--- hotel.c
+++ hotel.c
@@ -529,15 +529,16 @@ void creer_reservation()
 {
   cible_date()    ;
   cible_chambre() ;
-  /*
   rech_periode(date_debut, date_fin ) ;
+  /*
   continue_resa = choix_chambre() ; /*À voir si choix chambre récupère des arguments ou bien si rech_periode affecte des variables globales*//*
   if(continue_resa)
   {
     saisie_client();
     paiement_resa();
     maj_planning   ;
-  }*/
+  }
+  */
 }
 
 /*############################################
