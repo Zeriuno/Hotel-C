5e6fcb2d5035dbc904601b6888b9988855a512d2
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-02-16 02:30:47 +0100

    DOSSIER_RESA

--- declarations.h
+++ declarations.h
@@ -19,3 +19,4 @@
 #define PRIX_NUIT "1-Parametres/prix_nuits.txt" /*Nom du fichier contenant le prix d'une nuitée*/
 #define RESA_CODE "1-Parametres/resa-code.txt" /*Nom du fichier qui contient le numéro de la dernière réservation faite. À charger au démarrage via long unsigned int dernier_code_resa() qui affecte nb_resa.
 Le code 0 indique que la chambre est libre, le code 1 est réservé pour la déclaration de travaux.*/
+#define  DOSSIER_RESA "resa/" /* Dossier dans lequel se trouvent les réservations. Si cette information change, il faut songer à modifier la longueur des chaînes de caractères dans lesquelles elle est versée */
