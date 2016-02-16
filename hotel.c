929e8fd2dc78dfc450f65ae84d6abeaa42398ab6
Zeriuno <d.pitrolo@gmx.com>
2016-02-16 11:25:22 +0100

    Suivi des tâches

--- hotel.c
+++ hotel.c
@@ -32,9 +32,7 @@ Reste à faire:
 * optimiser enregistrement sur fichier et cycle de modification des chambres.
 * optimiser affichage et saisie des descripteurs des chambres.
 * évaluer la possibilité de déclarer les variables de choix (vue/sans vue; fumeur/non fumeur; haute saison/basse saison) comme des short int;
-* tests.
-* void lecture_jours() à améliorer pour récomposer la date en format jj/mm/aaaa (cf numsecsoc de JM Léry)
-* maj_calendrier() ajouter: tests sur les valeurs saisies et conversion aaaammjj en jj/mm/aaaa
+* tests détaillés dans chaque sous-programme.
 
 */
 
