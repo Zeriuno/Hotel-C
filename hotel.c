ea83bb2ce620e7cbbce0339cbbe8104572eaa11b
Zeriuno <d.pitrolo@gmx.com>
2016-02-16 22:47:03 +0100

    Test des valeurs saisies dans paiement_resa()

--- hotel.c
+++ hotel.c
@@ -1150,7 +1150,7 @@ void nouveau_nb_resa()
 void paiement_resa()
 {
   char date_1[11], date_2[11], temporaire[5]             ;
-  int i, test = 0, a, m, j                               ;
+  int i, test = 0, a, m, j, t2                           ;
   struct prix_nuit nuit                                  ;
   FILE *f1;
 
@@ -1160,73 +1160,91 @@ void paiement_resa()
   i= -1;
   while (test == 0) /* On retrouve le prix à la nuitée en trouvant dans le tableau des prix le champ où type de chambre et catégorie sont les mêmes que dans notre cas */
   {
-    i++                                                  ;
+    i++                                                    ;
     if ((tab_prix_chambres[i].type_chambre==nuit.type_chambre) && (tab_prix_chambres[i].categorie_chambre==nuit.categorie_chambre))
 
     {
-      nuit=tab_prix_chambres[i]                          ;
-      test = 1                                           ;
+      nuit=tab_prix_chambres[i]                            ;
+      test = 1                                             ;
     }
   }
   demande.total_resa=(demande.nuitees_resa[0]*nuit.prix_bs)+(demande.nuitees_resa[1]*nuit.prix_hs) ;
 
-  date_1[0] = '\0'                                       ;
-  temporaire[0] = '\0'                                   ;
-  a = demande.datearrivee/10000                          ;
-  m = (demande.datearrivee - (a * 10000)) / 100          ;
-  j = (demande.datearrivee - (a * 10000) - (m * 100))    ;
-  sprintf(temporaire, "%d", j)                           ;
-  strcat(date_1, temporaire)                             ;
-  strcat(date_1, "/")                                    ;
-  sprintf(temporaire, "%d", m)                           ;
-  strcat(date_1, temporaire)                             ;
-  strcat(date_1, "/")                                    ;
-  sprintf(temporaire, "%d", a)                           ;
-  strcat(date_1, temporaire)                             ;
+  date_1[0] = '\0'                                         ;
+  temporaire[0] = '\0'                                     ;
+  a = demande.datearrivee/10000                            ;
+  m = (demande.datearrivee - (a * 10000)) / 100            ;
+  j = (demande.datearrivee - (a * 10000) - (m * 100))      ;
+  sprintf(temporaire, "%d", j)                             ;
+  strcat(date_1, temporaire)                               ;
+  strcat(date_1, "/")                                      ;
+  sprintf(temporaire, "%d", m)                             ;
+  strcat(date_1, temporaire)                               ;
+  strcat(date_1, "/")                                      ;
+  sprintf(temporaire, "%d", a)                             ;
+  strcat(date_1, temporaire)                               ;
 
-  date_2[0] = '\0'                                       ;
-  temporaire[0] = '\0'                                   ;
-  a = demande.datedepart/10000                           ;
-  m = (demande.datedepart - (a * 10000)) / 100           ;
-  j = (demande.datedepart - (a * 10000) - (m * 100))     ;
-  sprintf(temporaire, "%d", j)                           ;
-  strcat(date_2, temporaire)                             ;
-  strcat(date_2, "/")                                    ;
-  sprintf(temporaire, "%d", m)                           ;
-  strcat(date_2, temporaire)                             ;
-  strcat(date_2, "/")                                    ;
-  sprintf(temporaire, "%d", a)                           ;
-  strcat(date_2, temporaire)                             ;
+  date_2[0] = '\0'                                         ;
+  temporaire[0] = '\0'                                     ;
+  a = demande.datedepart/10000                             ;
+  m = (demande.datedepart - (a * 10000)) / 100             ;
+  j = (demande.datedepart - (a * 10000) - (m * 100))       ;
+  sprintf(temporaire, "%d", j)                             ;
+  strcat(date_2, temporaire)                               ;
+  strcat(date_2, "/")                                      ;
+  sprintf(temporaire, "%d", m)                             ;
+  strcat(date_2, temporaire)                               ;
+  strcat(date_2, "/")                                      ;
+  sprintf(temporaire, "%d", a)                             ;
+  strcat(date_2, temporaire)                               ;
 
 
-  printf("Réservation n.%lu\n", demande.code_resa)       ;
+  printf("Réservation n.%lu\n", demande.code_resa)         ;
   printf("Client : %s %s\n", demande.nomclient, demande.prenomclient)    ;
-  printf("Séjour du %s au %s\n", date_1, date_2 )        ;
-  printf("Chambre n.%d\n", demande.chambre_resa)         ;
+  printf("Séjour du %s au %s\n", date_1, date_2 )          ;
+  printf("Chambre n.%d\n", demande.chambre_resa)           ;
   if(demande.nuitees_resa[0] > 0)
   {
     printf("Nuit en tarif basse saison : %d\n", demande.nuitees_resa[0]) ;
-    printf("Tarif par nuit : %.2f\n", nuit.prix_bs)      ;
+    printf("Tarif par nuit : %.2f\n", nuit.prix_bs)        ;
   }
   if(demande.nuitees_resa[1] > 0)
   {
     printf("Nuit en tarif haute saison : %d\n", demande.nuitees_resa[1]) ;
-    printf("Tarif par nuit : %.2f\n", nuit.prix_hs)      ;
+    printf("Tarif par nuit : %.2f\n", nuit.prix_hs)        ;
   }
-
-  printf("Montant à payer : %.2f\n", demande.total_resa) ;
-  printf("Choisir le mode de paiement: \n")              ;
-  printf("-1- Espèces\n")                                ;
-  printf("-2- Chèque\n")                                 ;
-  printf("-3- Carte bancaire\n")                         ;
-  printf("-4- Virement\n")                               ;
-  printf("Choix : ")                                     ;
-  scanf("%d", &demande.mode_paiement)                    ;
-  if(demande.mode_paiement == 3)
+  test = 0 ;
+  while(test == 0)
   {
-    paiement_cb()                                        ;
+    printf("Montant à payer : %.2f\n", demande.total_resa) ;
+    printf("Choisir le mode de paiement: \n")              ;
+    printf("-1- Espèces\n")                                ;
+    printf("-2- Chèque\n")                                 ;
+    printf("-3- Carte bancaire\n")                         ;
+    printf("-4- Virement\n")                               ;
+    printf("Choix : ")                                     ;
+    t2 = scanf("%d", &demande.mode_paiement)               ;
+    if(t2 == 0)
+    {
+      printf("Erreur de saisie.\n")                        ;
+    }
+    else
+    {
+      if((demande.mode_paiement < 0) || (demande.mode_paiement > 4))
+      {
+        mauvais_choix(demande.mode_paiement)               ;
+      }
+      else
+      {
+        if(demande.mode_paiement == 3)
+        {
+          paiement_cb()                                    ;
+        }
+        printf("Le paiement a bien été effectué.\n")       ;
+        test = 1                                           ;
+      }
+    }
   }
-  printf("Le paiement a bien été effectué.\n")           ;
 }
 
 
